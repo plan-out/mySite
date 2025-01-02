f9da137fdbb93d097406633ffb1ce4b41440ce8a
plan-out <45305408+plan-out@users.noreply.github.com>
2025-01-02 12:01:33 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const gnb_swiper = new Swiper("#gnb",{
+	wrapperClass:"menu",
+	slideClass:"btn",
+	slidesPerView:"auto", //버튼의 갯수만큼 설정
+});
+
+const main_swiper = new Swiper("main",{
+	wrapperClass:"container",
+	slideClass:"area",
+	direction:"vertical",
+	speed:800,
+	mousewheel:{
+		enabled:true,
+	},
+	thumbs:{
+		swiper:gnb_swiper,
+		slideThumbActiveClass:"active",
+	},
+});
